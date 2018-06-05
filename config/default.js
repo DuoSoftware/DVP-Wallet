96d80aaf4731d83f97a47c52084a80a8e50bde08
sine19 <sineth17@gmail.com>
2018-06-05 17:14:49 +0530

    clear all values
    remove all default.js and conf.js values

--- config/default.js
+++ config/default.js
@@ -1,22 +1,22 @@
 module.exports = {
     "DB": {
         "Type": "postgres",
-        "User": "duo",
-        "Password": "DuoS123",
+        "User": "",
+        "Password": "",
         "Port": 5432,
-        "Host": "104.236.231.11",
-        "Database": "duo"
+        "Host": "",
+        "Database": ""
     },
     "Redis": {
         "mode": "sentinel",//instance, cluster, sentinel
-        "ip": "45.55.205.92",
+        "ip": "",
         "port": 6389,
-        "user": "duo",
+        "user": "",
         "db": 0,
-        "password": "DuoS123",
+        "password": "",
         "ttl": 30000,
         "sentinels": {
-            "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+            "hosts": "",
             "port": 16389,
             "name": "redis-cluster"
         }
@@ -25,13 +25,13 @@ module.exports = {
 
     "Security": {
 
-        "ip": "45.55.205.92",
+        "ip": "",
         "port": 6389,
-        "user": "duo",
-        "password": "DuoS123",
+        "user": "",
+        "password": "",
         "mode": "sentinel",//instance, cluster, sentinel
         "sentinels": {
-            "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+            "hosts": "",
             "port": 16389,
             "name": "redis-cluster"
         }
@@ -46,21 +46,21 @@ module.exports = {
     },
 
     "RabbitMQ": {
-        "ip": "45.55.142.207",
+        "ip": "",
         "port": 5672,
-        "user": "guest",
-        "password": "guest"
+        "user": "",
+        "password": ""
     },
 
     "Services": {
 
-        "limitServiceHost": "192.168.0.54",
+        "limitServiceHost": "",
         "limitServicePort": 8084,
         "limitServiceVersion": "6.0",
         "trunkServiceHost": "192.168.0.89",
         "trunkServicePort": 9898,
         "trunkServiceVersion": "1.0.0.0",
-        "voxboneUrl": "https://sandbox.voxbone.com/ws-voxbone/services/rest"
+        "voxboneUrl": ""
 
     }
 };
